250053099835705b9c8f22cdf3601a002616397b
Emir De La Cruz <emir091305@gmail.com>
2024-06-10 20:34:42 -0500

    clase 1 js

---
+++ variables.js
@@ -0,0 +1,31 @@
+//alert("hola")
+// variables y tipos de datos
+let nombre = "emir";
+let apellido = "dlc";
+
+//alert( nombre);
+//alert( apellido);
+console.log(nombre);
+const dni = "71749445";
+console.log(dni);
+nombre = "agustin";
+console.log(nombre);
+let edad = 31;
+console.log(edad);
+//let numero= prompt("ingresa un numero")
+//console.log(numero);
+//let algoMas=prompt("escribe tu nombre")
+//console.log("hola " + algoMas + " como estas?")
+
+let n1 = 5;
+let n2 = 10;
+let total = n1 + n2;
+console.log(total);
+
+let n3 = "5";
+let n4 = "10";
+let totalD = n3 + n4;
+console.log(totalD);
+
+let x = "12";
+console.log(x);
